b96379fc1969eaae41e71ecd0367c8f35992cc17
AdsonSantiago <103952328+AdsonSantiago@users.noreply.github.com>
2023-09-12 16:23:25 -0300

    Update script.js

--- static/javas/script.js
+++ static/javas/script.js
@@ -1,161 +1,206 @@
-
-// campo pesquisa
-document.getElementById("pesquisa").addEventListener("click", function() {
-  var products = [];
-  var checkboxes = document.getElementsByName("products");
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      products.push(checkboxes[i].value);
-    }
-  }
-  alert("Produtos selecionados: " + products.join(", "));
-});
-
 // Função para calcular o preço total
 function calculateTotal() {
     var checkboxes = document.querySelectorAll('input[name="products"]:checked');
     var totalPrice = 0;
-  
-    checkboxes.forEach((checkbox) => {
-      if (checkbox.value.includes("-")) {
-        var pricePart = checkbox.value.split(" - ")[1]; // Obtém a parte do preço
-  
-        if (/^R\$\d+\.\d{2}$/.test(pricePart)) {
-          // Remove "R$ " e converte para um número
-          var price = parseFloat(pricePart.replace("R$", ""));
-          totalPrice += price;
-        } else {
-          console.error("Formato de valor inválido:", pricePart);
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.value.includes("-")) {
+            var pricePart = checkbox.value.split(" - ")[1]; // Obtém a parte do preço
+            
+            if (/^R\$\d+\.\d{2}$/.test(pricePart)) {
+                // Remove "R$ " e converte para um número
+                var price = parseFloat(pricePart.replace("R$", ""));
+                totalPrice += price;
+            } else {
+                console.error("Formato de valor inválido:", pricePart);
+            }
         }
-      }
     });
-  
+
     return totalPrice.toFixed(2);
-  }
-  
-  // Função para gerar a página de orçamento
-  function generateBudget() {
+}
+
+// Função para gerar a página de orçamento
+function generateBudget() {
     const selectedProducts = document.querySelectorAll('input[name="products"]');
-    const result = document.getElementById("result");
-    const produtosSelecionados = document.getElementById("selectedProducts");
-    const totalPriceElement = document.getElementById("totalPrice");
-  
+    const observations = document.querySelectorAll('input[name="observations"]'); // Seleciona todos os campos de observação
+
+    const result = document.getElementById('result');
+    const produtosSelecionados = document.getElementById('selectedProducts');
+    const totalPriceElement = document.getElementById('totalPrice');
+
     // Limpa a lista de produtos selecionados
-    produtosSelecionados.innerHTML = "";
-  
+    produtosSelecionados.innerHTML = '';
+
     var totalPrice = 0; // Inicializa o preço total
-  
+
     const produtosSelecionadosArray = []; // Para armazenar os produtos selecionados
-  
-    selectedProducts.forEach((input) => {
-      var productName =
-        input.parentElement.querySelector(".produto-text").textContent;
-      var quantity = parseInt(input.value);
-      var pricePart = input.parentElement
-        .querySelector(".produto-text")
-        .textContent.split(" - ")[1];
-  
-      if (/^R\$\d+\.\d{2}$/.test(pricePart)) {
-        var price = parseFloat(pricePart.replace("R$", ""));
-        var subtotal = price * quantity;
-  
-        if (quantity > 0) {
-          // Cria um elemento de lista para o produto selecionado com a quantidade
-          var listItem = document.createElement("li");
-          listItem.textContent = productName + " - Qntd: " + quantity;
-          produtosSelecionados.appendChild(listItem);
-  
-          totalPrice += subtotal; // Adiciona o subtotal ao preço total
-  
-          // Adicione o produto ao array de produtos selecionados
-          produtosSelecionadosArray.push({
-            name: productName,
-            quantity: quantity,
-          });
+
+    selectedProducts.forEach((input, index) => {
+        var productName = input.parentElement.querySelector('.produto-text').textContent;
+        var quantity = parseInt(input.value);
+        var pricePart = input.parentElement.querySelector('.produto-text').textContent.split(" - ")[1];
+        
+        // Acesse a observação correspondente ao produto atual
+        var observationInput = observations[index];
+        var observation = observationInput ? observationInput.value : ''; // Obtém a observação correspondente
+
+        if (/^R\$\d+\.\d{2}$/.test(pricePart)) {
+            var price = parseFloat(pricePart.replace("R$", ""));
+            var subtotal = price * quantity;
+
+            if (quantity > 0) {
+                // Cria um elemento de lista para o produto selecionado com a quantidade e a observação
+                var listItem = document.createElement('li');
+                listItem.textContent = productName + ' / Qntd: ' + quantity + ' / Obs: ' + observation;
+                produtosSelecionados.appendChild(listItem);
+
+                totalPrice += subtotal; // Adiciona o subtotal ao preço total
+
+                // Adicione o produto ao array de produtos selecionados
+                produtosSelecionadosArray.push({ name: productName, quantity: quantity, observation: observation });
+            }
+        } else {
+            console.error("Formato de valor inválido:", pricePart);
         }
-      } else {
-        console.error("Formato de valor inválido:", pricePart);
-      }
     });
-  
+    
     // Exibe o preço total
     totalPriceElement.textContent = totalPrice.toFixed(2);
-  
+
     // Mostra a página de resultados
-    result.style.display = "block";
-  
-    return {
-      produtosSelecionados: produtosSelecionadosArray,
-      totalPrice: totalPrice.toFixed(2),
-    };
-  }
-  
-  // Associa a função de geração de orçamento ao botão
-  const generateButton = document.getElementById("generateButton");
-  generateButton.addEventListener("click", generateBudget);
-  
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // pdf.js
-  function gerarPDF(produtosSelecionados, totalPrice) {
+    result.style.display = 'block';
+
+    return { produtosSelecionados: produtosSelecionadosArray, totalPrice: totalPrice.toFixed(2) };
+}
+
+
+// Associa a função de geração de orçamento ao botão
+const generateButton = document.getElementById('generateButton');
+generateButton.addEventListener('click', generateBudget);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// pdf.js
+
+function gerarPDF(produtosSelecionados, totalPrice) {
+    console.log(totalPrice)
     var doc = new jsPDF();
     var logo = new Image();
-    logo.src =
-      "https://cdn.glitch.global/6734fcaa-1157-4b47-9cdf-898c918a353e/vivo-120.png?v=1692977465269";
-    var titulo = new Image();
-    titulo.src =
-      "https://cdn.glitch.global/6734fcaa-1157-4b47-9cdf-898c918a353e/logo-titulo.png?v=1693333031528";
-  
-    // https://cdn.glitch.global/6734fcaa-1157-4b47-9cdf-898c918a353e/vivo-120.png?v=1692977465269
-  
-    logo.onload = function () {
-      doc.addImage(logo, "PNG", 170, 270, 30, 20);
-  
-      doc.text("ORÇAMENTO CASA INTELIGENTE", 105, 15, null, null, "center");
-  
-      doc.setFont("helvetica"); // Nome da fonte, você pode substituir por outras fontes suportadas
-      doc.text("Produtos - Preços:", 10, 35);
-  
-      // Inicialize uma string para a lista de produtos
-      var produtosText = "";
-  
-      produtosSelecionados.forEach((produto, index) => {
+    
+    // Obtenha os elementos de entrada para Solicitante
+    var nomeSolicitante = document.getElementById('nomeSolicitante').value;
+    var telefoneSolicitante = document.getElementById('telSolicitante').value;
+    var emailSolicitante = document.getElementById('emailSolicitante').value;
+    
+    // Obtenha os elementos de entrada para Cliente
+    var nomeCliente = document.getElementById('nomeCliente').value;
+    var telefoneCliente = document.getElementById('telCliente').value;
+    var emailCliente = document.getElementById('emailCliente').value;
+    
+    
+    logo.src = 'https://cdn.glitch.global/6734fcaa-1157-4b47-9cdf-898c918a353e/vivo-120.png?v=1692977465269';
+
+    doc.addImage(logo, 'PNG', 170, 270, 30, 20);
+
+    doc.text('ORÇAMENTO CASA INTELIGENTE', 105, 15, null, null, 'center');
+
+    // Adicione campos Solicitante
+    doc.setFont('helvetica');
+    doc.setFontSize(12);
+
+    doc.setFontStyle('bold');
+    doc.text('Solicitante', 10, 35);
+    doc.text('Cliente', 100, 35);
+    doc.setFontStyle('normal');
+
+    doc.text('Nome: ' + nomeSolicitante, 10, 45);
+    doc.text('Telefone: ' + telefoneSolicitante, 10, 50);
+    doc.text('Email: ' + emailSolicitante, 10, 55);
+    
+    // Adicione campos Cliente
+    doc.text('Nome: ' + nomeCliente, 100, 45);
+    doc.text('Telefone: ' + telefoneCliente, 100, 50);
+    doc.text('Email: ' + emailCliente, 100, 55);
+
+    // Inicialize uma string para a lista de produtos
+    var produtosText = '';
+
+    produtosSelecionados.forEach((produto, index) => {
         // Adicione cada produto ao PDF
-        produtosText += `${index + 1}. ${produto.name}\n`;
-      });
-  
-      // Defina a coordenada vertical (y) para a lista de produtos
-      var yPos = 50;
-      doc.setFont("helvetica"); // Nome da fonte, você pode substituir por outras fontes suportadas
-      doc.setFontType("normal"); // Estilo da fonte (normal, bold, italic, bolditalic)
-  
-      // Definir o tamanho da fonte
-      doc.setFontSize(12); // Tamanho da fonte em pontos
-      // Adicione a lista de produtos ao PDF
-      doc.text(produtosText, 10, yPos, { fontSize: 8 }); // Aqui, o tamanho da fonte
-      yPos += 10; // Ajuste a coordenada vertical conforme necessário
-  
-      doc.setFontStyle("bold"); // Defina o estilo para negrito
-      doc.setFontSize(14);
-      // Adicione o texto do preço total como resumo no final do PDF
-      doc.text(`Preço Total: R$ ${totalPrice}`, 10, yPos + 195);
-      doc.setFontStyle("normal");
-  
-      // Crie uma linha horizontal
-      doc.setLineWidth(0.5); // Defina a largura da linha como 2 (aumente ou diminua conforme desejado)
-      doc.setDrawColor(128, 0, 128); // Defina a cor da linha como roxa (128, 0, 128 é a representação de roxo no formato RGB)
-      doc.line(10, 265, 200, 265); // Desenhe a linha com as configurações definidas
-  
-      var base64String = doc.output("datauristring");
-      window.open(base64String, "_blank");
-    };
-  }
-  
-  // Adicione um ouvinte de eventos ao botão "otherButton"
-  const otherButton = document.getElementById("otherButton");
-  otherButton.addEventListener("click", function () {
+        produtosText += `${index + 1}. ${produto.name} / Qntd: ${produto.quantity}\n Obs: ${produto.observation}\n \n`;
+    });
+
+    // Defina a coordenada vertical (y) para a lista de produtos
+    var yPos = 80;
+    doc.setFont('helvetica');
+    doc.setFontType('normal');
+    doc.setFontStyle('bold');
+    doc.text('Produtos - Preços:', 10, yPos);
+    doc.setFontStyle('normal');
+
+    // Adicione a lista de produtos ao PDF
+    doc.text(produtosText, 10, yPos + 10, { fontSize: 8 });
+    yPos += 10;
+
+    doc.setFontStyle('bold');
+    doc.setFontSize(14);
+
+    doc.text(`Preço Total: R$ ${totalPrice}`, 10, yPos + 167);
+    doc.setFontStyle('normal')
+
+
+    doc.setLineWidth(0.5);
+    doc.setDrawColor(128, 0, 128);
+    doc.line(10, 265, 200, 265);
+
+    var base64String = doc.output('datauristring');
+    window.open(base64String, '_blank');
+    doc.save('teste.pdf');
+
+};
+
+
+// Adicione um ouvinte de eventos ao botão "otherButton"
+const otherButton = document.getElementById('otherButton');
+otherButton.addEventListener('click', function () {
     const { produtosSelecionados, totalPrice } = generateBudget();
-  
+
     // Chame a função gerarPDF com os produtosSelecionados e o totalPrice
     gerarPDF(produtosSelecionados, totalPrice);
-  });
-  
+});
+
+
+function filterProducts() {
+    const searchTerm = document.getElementById("pesquisa").value.toLowerCase();
+    const productContainers = document.getElementsByClassName("produto-container");
+
+    for (let i = 0; i < productContainers.length; i++) {
+        const productName = productContainers[i].querySelector(".produto-text").textContent.toLowerCase();
+
+        if (productName.includes(searchTerm)) {
+            // Remove a classe "hidden-product" para mostrar o produto correspondente
+            productContainers[i].classList.remove("hidden-product");
+        } else {
+            // Adicione a classe "hidden-product" para ocultar o produto que não corresponde
+            productContainers[i].classList.add("hidden-product");
+        }
+    }
+}
+
+
+// Função para mostrar/ocultar o botão com base na posição da página
+window.onscroll = function() {
+    var topButton = document.getElementById("topButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+};
+
+// Função para rolar suavemente para o topo quando o botão é clicado
+document.getElementById("topButton").onclick = function() {
+    document.body.scrollTop = 0; // Para navegadores mais antigos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
+};
